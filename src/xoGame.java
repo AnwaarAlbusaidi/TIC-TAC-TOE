674d38a4c54cd292a2e6c3ceae928bec3347fce9
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2022-12-12 23:18:53 -0800

    Done

--- src/xoGame.java
+++ src/xoGame.java
@@ -25,16 +25,14 @@ public class xoGame {
 		int rowIndex;
 		int columIndex;
 
-		print(grid);
-
 		while (playgame == true) {
-
+			print(grid);
 			if (Player1) // check if it is player 1 turn
 			{
 				System.out.println("Player 1, Make a move(symbol : x), Enter postion: ");
 				playerChoice = scan.nextInt();
 				if (playerChoice <= 3) {
-					rowIndex = 0;
+					rowIndex = 0;  //if input is 1 or 2 or 3 that mean we are in row 0
 					columIndex = playerChoice - 1;
 					if (grid[rowIndex][columIndex] == 'x' || grid[rowIndex][columIndex] == 'o') {
 						System.out.println("Someone has already made a move at this position! Try again.");
@@ -48,11 +46,10 @@ public class xoGame {
 							System.out.println("Player 2 is the winner");
 							playgame = false;
 						}
-						print(grid);
 					}
 
 				} else if (playerChoice <= 6) {
-					rowIndex = 1;
+					rowIndex = 1;   //if input is 4 or 5 or 6 that mean we are in row 1
 					columIndex = playerChoice - 4;
 					if (grid[rowIndex][columIndex] == 'x' || grid[rowIndex][columIndex] == 'o') {
 						System.out.println("Someone has already made a move at this position! Try again.");
@@ -66,10 +63,9 @@ public class xoGame {
 							System.out.println("Player 2 is the winner");
 							playgame = false;
 						}
-						print(grid);
 					}
 				} else if (playerChoice <= 9) {
-					rowIndex = 2;
+					rowIndex = 2;  //if input is 7 or 8 or 9 that mean we are in row 2
 					columIndex = playerChoice - 7;
 					if (grid[rowIndex][columIndex] == 'x' || grid[rowIndex][columIndex] == 'o') {
 						System.out.println("Someone has already made a move at this position! Try again.");
@@ -83,7 +79,6 @@ public class xoGame {
 							System.out.println("Player 2 is the winner");
 							playgame = false;
 						}
-						print(grid);
 					}
 				}
 
@@ -97,13 +92,13 @@ public class xoGame {
 				System.out.println("Player 2, Make a move(symbol : 0), Enter postion: ");
 				playerChoice = scan.nextInt();
 				if (playerChoice <= 3) {
-					rowIndex = 0;
+					rowIndex = 0;  //if input is 1 or 2 or 3 that mean we are in row 0
 					columIndex = playerChoice - 1;
 					if (grid[rowIndex][columIndex] == 'x' || grid[rowIndex][columIndex] == 'o') {
 						System.out.println("Someone has already made a move at this position! Try again.");
 						Player1 = false;
 					} else {
-						grid[rowIndex][columIndex] = '0';
+						grid[rowIndex][columIndex] = 'o';
 						if (winner(grid) == 'x') {
 							System.out.println("Player 1 is the winner");
 							playgame = false;
@@ -113,16 +108,15 @@ public class xoGame {
 						} else
 							Player1 = true;
 					}
-					print(grid);
 
 				} else if (playerChoice <= 6) {
-					rowIndex = 1;
+					rowIndex = 1;    //if input is 4 or 5 or 6 that mean we are in row 1
 					columIndex = playerChoice - 4;
 					if (grid[rowIndex][columIndex] == 'x' || grid[rowIndex][columIndex] == 'o') {
 						System.out.println("Someone has already made a move at this position! Try again.");
 						Player1 = false;
 					} else {
-						grid[rowIndex][columIndex] = '0';
+						grid[rowIndex][columIndex] = 'o';
 						if (winner(grid) == 'x') {
 							System.out.println("Player 1 is the winner");
 							playgame = false;
@@ -132,15 +126,14 @@ public class xoGame {
 						} else
 							Player1 = true;
 					}
-					print(grid);
 				} else if (playerChoice <= 9) {
-					rowIndex = 2;
+					rowIndex = 2;    //if input is 7 or 8 or 9 that mean we are in row 2
 					columIndex = playerChoice - 7;
 					if (grid[rowIndex][columIndex] == 'x' || grid[rowIndex][columIndex] == 'o') {
 						System.out.println("Someone has already made a move at this position! Try again.");
 						Player1 = false;
 					} else {
-						grid[rowIndex][columIndex] = '0';
+						grid[rowIndex][columIndex] = 'o';
 						if (winner(grid) == 'x') {
 							System.out.println("Player 1 is the winner");
 							playgame = false;
@@ -150,18 +143,8 @@ public class xoGame {
 						} else
 							Player1 = true;
 					}
-					print(grid);
 				}
 			} // End of else
-
-			if (winner(grid) == 'x') {
-				System.out.println("Player 1 is the winner");
-				playgame = false;
-			} else if (winner(grid) == 'o') {
-				System.out.println("Player 2 is the winner");
-				playgame = false;
-			}
-
 		} // End of while
 	}// End of Main class
 
@@ -196,7 +179,7 @@ public class xoGame {
 		if (g[0][2] == g[1][1] && g[1][1] == g[2][0]) // check dislodge
 			return g[1][1];
 
-		return ' ';
+		return 'D';
 	}// end for winner method
 
 }// End of xoGame class
